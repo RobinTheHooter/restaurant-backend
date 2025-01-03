6e607a2afc88f605e17e2fee6e3497efb88c4daf
Jatin Chandra Choudhary <98642385+RobinTheHooter@users.noreply.github.com>
2025-01-03 16:49:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(
       process.env.NODE_ENV === "production"
         ? process.env.FRONTEND_URL
         : "http://localhost:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(express.json());
